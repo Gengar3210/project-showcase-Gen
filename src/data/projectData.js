d36340f796b42f030b2190afab7eeff48b32d166
Gengar3210 <hutao.gengar814@gmail.com>
2025-08-21 11:17:00 -1200

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,18 +1,30 @@
-// src/data/projectData.js
 export const projects = [
   {
     id: 1,
     title: {
-      en: "",
-      es: "",
+      en: "Speak Clearly",
+      es: "Habla Claro",
     },
     description: {
-      en: "",
-      es: "",
+      en: "Hey, you! Check out my project below.",
+      es: "Oye, tú! Echa un vistazo a mi proyecto a continuación.",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://gengar3210.github.io/Habla-Claro/",
+    githubRepo: "https://gengar3210.github.io/Habla-Claro/",
   },
-  // Add more projects here...
+ {
+    id: 2,
+    title: {
+      en: "Unknown",
+      es: "Desconocido",
+    },
+    description: {
+      en: "Hey, you! Check out my project below.",
+      es: "Oye, tú! Echa un vistazo a mi proyecto a continuación.",
+    },
+    image: "",
+    liveSite: "https://gengar3210.github.io/Habla-Claro/",
+    githubRepo: "https://gengar3210.github.io/Habla-Claro/",
+  },
 ];
